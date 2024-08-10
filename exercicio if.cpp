8e9ca8235efedf912e501eec99336c30962eed03
GuiSousa1804 <guilhermesousa1804@gmail.com>
2024-08-09 22:02:50 -0300

    Add files via upload

---
+++ exercicio if.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(){
+   int maior, menor, valor;
+   printf("Digite o primeiro numero");
+   scanf("%d",&valor);
+   maior = valor;
+   menor = valor;
+   printf("Digite o segundo numero");
+   scanf("%d",&valor);
+   if(valor > maior){
+   	  maior = valor;
+   }
+   if(valor < menor){
+   	  menor = valor;
+   }
+   printf("Digite o terceiro numero");
+   scanf("%d",&valor);
+   if(valor > maior){
+   	  maior = valor;
+   }
+   if(valor < menor){
+   	  menor = valor;
+   }
+   printf("Digite o quarto numero");
+   scanf("%d",&valor);
+   if(valor > maior){
+   	  maior = valor;
+   }
+   if(valor < menor){
+   	  menor = valor;
+   }
+   printf("Digite o quinto numero");
+   scanf("%d",&valor);
+   if(valor > maior){
+   	  maior = valor;
+   }
+   if(valor < menor){
+   	  menor = valor;
+   }
+    printf("Maior = %d e menor = %d",maior,menor);
+   return 0;
+}
